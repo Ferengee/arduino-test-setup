50e7de6d5dd9aa0d6451ca4d5ba17805d1ed9ecd
Bas Mesman <captainf@dds.nl>
2013-06-23 22:41:46 +0200

    renamed buffered managers' files

---
+++ test_string_reader/BufferManager.cpp
@@ -0,0 +1,27 @@
+#include <BufferManager.h>
+
+bool CharBufferManager::putchar(char item){
+  if (sizeLeft() > 0){
+    *_cursor = item; 
+    _cursor++;
+    _used++;
+    
+    return true;
+  }
+  return false;
+}
+
+void CharBufferManager::init(char * buffer, int len){
+  _buffer = buffer; 
+  _cursor = buffer;
+  _capacity = len;
+  _used = 0;
+}
+
+
+void CharBufferManager::terminateStr(){
+  if (sizeLeft() > 0)
+    *_cursor = 0; 
+  else
+    _buffer[_capacity -1] = 0;
+}
