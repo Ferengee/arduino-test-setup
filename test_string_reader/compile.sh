52ea1ef80f869862fa64a77278e3fcb359207694
Bas Mesman <captainf@dds.nl>
2013-06-24 21:06:24 +0200

    cleanup of state machine source

--- test_string_reader/compile.sh
+++ test_string_reader/compile.sh
@@ -1,1 +1,1 @@
-g++ -Wall StateMachine.cpp BufferManager.cpp -I . -I ../Arduino ../Arduino/DummySerial.cpp ../Arduino/Arduino.cpp -l ncurses
+g++ -Wall test_state_machine.cpp StateMachine.cpp BufferManager.cpp -I . -I ../Arduino ../Arduino/DummySerial.cpp ../Arduino/Arduino.cpp -l ncurses
