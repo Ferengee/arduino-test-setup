ababe256a267e2b720d1b1c988721b41533758cf
Bas Mesman <captainf@dds.nl>
2013-06-24 21:02:20 +0200

    added 2 managers

--- test_string_reader/BufferManager.h
+++ test_string_reader/BufferManager.h
@@ -1,13 +1,19 @@
 #ifndef CharBufferManager_h
 #define CharBufferManager_h
 
-#include <iostream>
 
-class CharBufferManager
+class BufferManager
+{
+public:
+  BufferManager(){}
+  virtual bool putchar(char item){ return false;}
+};
+
+class CharBufferManager : public BufferManager
 {
 public:
   int sizeLeft(){ return _capacity - _used; }
-  bool putchar(char item);
+  virtual bool putchar(char item);
   
   void init(char * buffer, int len);
   char * getBuffer(){ return _buffer; }
@@ -19,6 +25,33 @@ private:
   int _used;
   char * _cursor;
   char * _buffer;
+};
+
+class IntBufferManager : public BufferManager
+{
+public:
+  virtual bool putchar(char item);
+  
+  void init();
+  int getBuffer(){ return (int)_buffer; }
+  
+protected:
+
+  long _buffer;
+  int _sign;
+};
+
+class FloatBufferManager : public IntBufferManager
+{
+public:
+  virtual bool putchar(char item);
+  
+  void init();
+  float getBuffer();
+  
+private:
+  float _fraction;
+  bool _isFraction;
 };
 
 
