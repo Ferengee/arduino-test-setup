8e9a486957e5283bfa9def3904347fe970bb7a9d
Bas Mesman <captainf@dds.nl>
2013-07-07 22:53:39 +0200

    Implemented MiniRPC prepare now returns void so the user of the interface does not have to worry about returning a usefull value.

---
+++ Arduino/libraries/MiniRPC/MiniRPC.cpp
@@ -0,0 +1,257 @@
+#include "MiniRPC.h"
+
+MiniRPCDispatcher::MiniRPCDispatcher(StreamWrapper * wrapper)
+{
+  streamWrapper = wrapper;
+  
+  stringStreamParser.setStreamWrapper(wrapper);
+  stringStreamParser.setBufferManager(&charBufferManager);
+
+  intStreamParser.setStreamWrapper(wrapper);
+  intStreamParser.setBufferManager(&intBufferManager);
+
+  floatStreamParser.setStreamWrapper(wrapper);
+  floatStreamParser.setBufferManager(&floatBufferManager);
+  
+  select(&methodMatcher);
+  
+}
+
+bool MiniRPCDispatcher::registerMethod(MiniRPCMethod* method)
+{
+  bool succes = _methodsCount < MAX_METHOD_COUNT;
+  if (succes){
+    _methods[_methodsCount++] = method;
+    method->dispatcher = this;
+  }
+  return succes;
+}
+
+void MiniRPCDispatcher::select(char* method_name, int length)
+{
+  int i;
+  bool found = false;
+  for (i=0; i < _methodsCount; i++){
+    if (strcmp(method_name, _methods[i]->getName()) == 0){
+      found = true;
+      select(_methods[i]);
+      break;
+    }  
+  }
+  if(!found){
+    Serial.println("failed to find:");
+  
+    Serial.println(method_name);
+  }
+}
+
+void MiniRPCDispatcher::select(MiniRPCMethod* method)
+{
+  selected_method = method;
+  //reset();
+  method->dispatcher = this;
+  method->init();
+  
+}
+
+void MiniRPCDispatcher::reset()
+{
+  stringStreamParser.reset();
+  intStreamParser.reset();
+  floatStreamParser.reset();
+
+}
+
+
+bool MiniRPCDispatcher::update()
+{
+  MiniRPCMethod * active_method = selected_method;
+  if(active_method->ready()){
+      active_method->execute();
+      /*
+       * the called method can update the dispatchers state 
+       */
+      if(active_method == selected_method){
+        select(&methodMatcher);
+      }
+  }else{
+    active_method->pre_prepare();
+    active_method->prepare();
+    if (active_method->error())
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+bool MiniRPCDispatcher::test(){
+int state;
+  charBufferManager.init(_test_value,30);
+  stringStreamParser.reset();
+  while((state = stringStreamParser.process()) == 0){
+    Serial.print("_"); 
+  }
+  if (state == -1){
+    Serial.println("Error");
+  }else{
+    Serial.println("Done");
+    charBufferManager.terminateStr();
+    Serial.println(charBufferManager.getBuffer());
+  }
+  return false;
+}
+
+MiniRPCMethod::MiniRPCMethod()
+{
+  active_argument_state = MINIRPC_ARGUMENT_INIT;
+  active_argument_index = 0;
+  current_argument_index = 0;
+}
+
+void MiniRPCMethod::pre_prepare()
+{
+  current_argument_index = -1;
+}
+
+void MiniRPCMethod::init()
+{
+  active_argument_state = MINIRPC_ARGUMENT_INIT;
+  active_argument_index = 0;
+  current_argument_index= 0;
+}
+
+
+int MiniRPCMethod::getStage()
+{
+  current_argument_index++;
+  if(current_argument_index == active_argument_index)
+    return active_argument_state;
+  else
+    return MINIRPC_ARGUMENT_SKIP;
+      
+}
+
+bool MiniRPCMethod::get(char* buffer, int len)
+{
+  
+  int mg = getStage();
+  switch(mg){
+    case MINIRPC_ARGUMENT_SKIP:
+      return false;
+
+    case MINIRPC_ARGUMENT_INIT:
+   
+      dispatcher->stringStreamParser.reset();
+      dispatcher->charBufferManager.init(buffer, len);
+      active_argument_state = MINIRPC_ARGUMENT_WORKING;
+  }
+  
+  /*
+    * clock the parser unless it is in the end state
+    * if the parser is in the end state
+    *        update active_argument_index to current_argument_index + 1
+    *        set active_argument_state to MINIRPC_ARGUMENT_INIT
+    */
+  int progress = dispatcher->stringStreamParser.process();
+  while(progress == 0){
+    progress = dispatcher->stringStreamParser.process();
+  }
+  switch(progress){
+    case 1:
+      active_argument_index = current_argument_index+1;
+      active_argument_state = MINIRPC_ARGUMENT_INIT;
+      break;
+    case -1:
+      active_argument_state = MINIRPC_ARGUMENT_ERROR;
+  }
+  return progress == 1;
+}
+
+bool MiniRPCMethod::get(int arg)
+{
+  int mg = getStage();
+  switch(mg){
+    case MINIRPC_ARGUMENT_SKIP:
+      return false;
+
+    case MINIRPC_ARGUMENT_INIT:
+      dispatcher->intStreamParser.reset();
+      dispatcher->intBufferManager.init();
+      active_argument_state = MINIRPC_ARGUMENT_WORKING;
+  }
+ 
+
+  int progress = dispatcher->intStreamParser.process();
+  switch(progress){
+    case 1:
+      active_argument_index = current_argument_index+1;
+      active_argument_state = MINIRPC_ARGUMENT_INIT;
+      break;
+    case -1:
+      active_argument_state = MINIRPC_ARGUMENT_ERROR;
+  }
+  return progress == 1;
+}
+
+
+bool MiniRPCMethod::get(float arg)
+{
+  int mg = getStage();
+  switch(mg){
+    case MINIRPC_ARGUMENT_SKIP:
+      return false;
+
+    case MINIRPC_ARGUMENT_INIT:
+      dispatcher->floatStreamParser.reset();
+      dispatcher->floatBufferManager.init();
+      active_argument_state = MINIRPC_ARGUMENT_WORKING;
+  }
+
+  int progress = dispatcher->floatStreamParser.process();
+  switch(progress){
+    case 1:
+      active_argument_index = current_argument_index+1;
+      active_argument_state = MINIRPC_ARGUMENT_INIT;
+      break;
+    case -1:
+      active_argument_state = MINIRPC_ARGUMENT_ERROR;
+  }
+  return progress == 1;
+}
+
+/*
+ * the result of this method is incorrect if it is called inside the branch of a get(...) call
+ * The result is only valid after at least one call to prepare()
+ * It returns true if the active_argument_state == MINIRPC_ARGUMENT_INIT && active_argument_index == current_argument_index + 1
+ * which implies that all the arguments have succesfully been parsed
+ */
+bool MiniRPCMethod::ready()
+{
+  return active_argument_state == MINIRPC_ARGUMENT_INIT && active_argument_index == current_argument_index + 1;
+}
+
+MethodMatcher::MethodMatcher()
+{
+
+}
+
+
+void MethodMatcher::execute()
+{
+    Serial.print("calling dispatcher: ");
+    Serial.println(dispatcher->charBufferManager.getBuffer());
+  dispatcher->select(_method_name, _method_name_length); 
+}
+
+void MethodMatcher::prepare()
+{
+  if(get(_method_name, MAX_METHOD_NAME_LENGTH))
+  {
+    _method_name_length = dispatcher->charBufferManager.used();
+    dispatcher->charBufferManager.terminateStr();    
+  }
+  
+}
+
--- Arduino/libraries/MiniRPC/MiniRPC.h
+++ Arduino/libraries/MiniRPC/MiniRPC.h
@@ -1,11 +1,17 @@
 #ifndef MiniRPC_h
 #define MiniRPC_h
 #define MAX_METHOD_COUNT 16
+#define MAX_METHOD_NAME_LENGTH 20
+#define MINIRPC_ARGUMENT_ERROR -2
+#define MINIRPC_ARGUMENT_SKIP -1
+#define MINIRPC_ARGUMENT_INIT 0
+#define MINIRPC_ARGUMENT_WORKING 1
+#define MINIRPC_ARGUMENT_FINISHED 2
 
 #include <Arduino.h>
 #include "StreamWrapper.h"
-#include "../../../test_string_reader/BufferManager.h"
 #include "StreamParser.h"
+#include "BufferManager.h"
 
 class MiniRPCDispatcher;
 
@@ -13,57 +19,97 @@ class MiniRPCMethod
 {
 public:
   MiniRPCDispatcher * dispatcher;
+  MiniRPCMethod();
   /* returns true if finished getting */
   bool get(char * buffer, int len);
   bool get(int arg);
-  char * getName();
+  bool get(float arg);
+  const char * getName(){ return _name; }
   void setName(const char * name){ _name = name;}
   virtual void execute(){}
-  virtual bool prepare(){return true;}
+  virtual void prepare(){}
+  void pre_prepare();
+  bool ready();
+  bool error(){return active_argument_state == MINIRPC_ARGUMENT_ERROR;}
+  void init();
 private:
   const char * _name;
-  
+  // the argument (get call) which is currently beeing evaluated
+  int current_argument_index;
+  // the argument which is allowed to read the stream to become complete
+  int active_argument_state; // 0 => init, 1 => working, 2 => finished, -2 => error, -1 => skip
+  int active_argument_index;
+  int getStage();
   
 };
 
 class TestMethod : public MiniRPCMethod
 {
+public:
   TestMethod(){
     setName("test.method");
   }
-  virtual bool prepare(){
+  virtual void prepare(){
     get(arg1, 10);
     get(arg2);
-    return true;
   }
   virtual void execute(){
     Serial.print(getName());
     Serial.print(":");
     Serial.println(arg1);
     Serial.println(arg2);
-    
   }
 private:
   char arg1[10];
   int arg2;
 };
 
+
+class MethodMatcher : public MiniRPCMethod
+{
+public:
+  virtual void execute();
+  virtual void prepare();
+  MethodMatcher();
+protected:
+  char _method_name[MAX_METHOD_NAME_LENGTH];
+  int _method_name_length;
+};
+
 class MiniRPCDispatcher
 {
 public:
+  MiniRPCDispatcher(StreamWrapper* wrapper);
+  MiniRPCDispatcher(){}
   /* returns true on succes */
   bool registerMethod(MiniRPCMethod * method);
+  /*
+   * call prepare on the current active method
+   * if it is done preparing, execute it
+   * after execution select the correct next active method
+   */
+  bool update();
+  void select(char * method_name, int length);
+  void select (MiniRPCMethod * method);
+  bool test();
+  
+  StreamWrapper * streamWrapper;
+  
+  CharBufferManager charBufferManager;
+  StringStreamParser stringStreamParser;
+  
+  IntBufferManager intBufferManager;
+  IntStreamParser intStreamParser;
+  
+  FloatBufferManager floatBufferManager;
+  FloatStreamParser floatStreamParser;
 private:
   MiniRPCMethod * _methods[MAX_METHOD_COUNT];
   int _methodsCount;
-  
-  StreamWrapper * streamWrapper;
-  CharBufferManager charBufferManager;
-  IntBufferManager intBufferManager;
-  FloatBufferManager floatBufferManager;
-  StringStreamParser stringParser;
-  
-  
+  MiniRPCMethod * selected_method;
+  MethodMatcher methodMatcher;
+  void reset();
+  char _test_value[30];
 };
 
 
