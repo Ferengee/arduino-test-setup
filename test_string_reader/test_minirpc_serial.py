c97f73c1da8780091e8abf38d674c3eba428282b
Bas Mesman <captainf@dds.nl>
2013-08-12 22:01:30 +0200

    python test for minirpc over serial uses separate thread for reading

--- test_string_reader/test_minirpc_serial.py
+++ test_string_reader/test_minirpc_serial.py
@@ -1,20 +1,34 @@
+#!/usr/bin/env python
 import serial
 import math
 import time
-import thread
+import threading
+import sys
 
-com = serial.Serial('/dev/ttyACM0')
+com = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
+running = True
+time.sleep(2)
+def readSerial(com):
+  while running:
+    sys.stdout.write(com.read())
 
-def readSerial():
-  while 1:
-    print(com.readline())
 
-thread.start_new_thread(readSerial, ())
+def test():
+  time.sleep(1)
 
-for i in range(100):
-  command = "led(11, %d)" % (math.sin(i) * 128 + 128)
-  com.write(command)
-  print(command)
-  time.sleep(0.3)
+  for i in range(100):
+    command = "led(11, %d)" % (math.sin(i) * 128 + 128)
+    com.write(command)
+    print(command)
+    time.sleep(0.1)
   
-    
+t1 = threading.Thread(target=readSerial, args=(com,))
+
+t1.start()
+com.write("led(11,200)")
+com.write("aap(11,200)")
+com.write("(((")
+test()
+time.sleep(2)
+running = False
+t1.join()
