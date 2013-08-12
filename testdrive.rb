2232993438d6137859e5d57293f1c3df1d6b5cf5
Bas Mesman <captainf@dds.nl>
2013-08-12 22:02:43 +0200

    don't write \n after each char

--- testdrive.rb
+++ testdrive.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/ruby
 puts "starting #{ARGV[0]}"
 process = IO.popen(ARGV[0], "w+")
-chars = %W{w d w w w d w d w w}
+chars = %W{"hello" 345|34.65}
 chars.push ""
 inp = ""
 while inp = process.gets do
   puts inp
   c = chars.shift
   puts c
-  process.puts c
-  sleep 10
+  process.write c
+  sleep 1
 end
