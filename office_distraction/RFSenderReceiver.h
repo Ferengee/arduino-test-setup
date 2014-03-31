c3d55832256503fc02bffdacc5d1dc2a7882830d
Bas Mesman <captainf@dds.nl>
2014-03-31 17:00:41 +0200

    Added test for the reply machine override RFSenderReceiver with stub

---
+++ office_distraction/RFSenderReceiver.h
@@ -0,0 +1,8 @@
+#ifndef RFSenderReceiver_h
+#define RFSenderReceiver_h
+#include <TestSenderReceiver.h>
+
+class RFSenderReceiver : public TestSenderReceiver{};
+
+
+#endif
---
+++ office_distraction/TestReplyStatemachine.cpp
@@ -0,0 +1,9 @@
+#define USEMAIN
+#include "/home/ferengee/src/arduino-libs/OfficeDistraction/examples/TestReplyStatemachine/TestReplyStatemachine.ino"
+
+#include "/home/ferengee/src/arduino-libs/OfficeDistraction/examples/TestReplyStatemachine/Statemachines.ino"
+
+int main(){
+  setup();
+  for(;;){loop();}
+}
