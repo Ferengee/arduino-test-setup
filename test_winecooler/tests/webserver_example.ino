b89ae008eb6dabf87b680e3dae8f15d11c483af1
Bas Mesman <captainf@dds.nl>
2016-06-29 21:12:59 +0200

    added ApiRequest example

--- test_winecooler/tests/webserver_example.ino
+++ test_winecooler/tests/webserver_example.ino
@@ -117,41 +117,38 @@ void setup() {
 void loop() {
   // listen for incoming clients
   EthernetClient client = server.available();
+  ApiRequest request;
+
   if (client) {
     Serial.println("new client");
     // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.print(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
+    request.setStream(&client);
+
+    request.initialize();
+    
+    int value = 17;
           // send a standard http response header
-          client.println("HTTP/1.1 200/OK");
-          client.println("Access-Control-Allow-Origin: * ");
-          client.println("Content-Type: text/plain");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println();
-          client.print("{\"temp\":");
-          temp->chainToJSON(output);
-          client.print(output);
-          client.print("}");
-         
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
+    client.println("HTTP/1.1 200/OK");
+    client.println("Access-Control-Allow-Origin: * ");
+    client.println("Content-Type: text/plain");
+    client.println("Connection: close");  // the connection will be closed after completion of the response
+    client.println();
+    client.print("{\"temp\":");
+      temp->chainToJSON(output);
+    client.print(output);
+    client.print(",\"key\":\"");
+    client.print(request.getKey());
+    client.print("\",\"id\":");
+    client.print(request.getInstanceId());
+    
+    client.print(",\"value\":");
+
+    if (request.method() != GET)
+      value = request.intData();
+    
+    client.print(value);
+    client.print("}");
+          
     delay(1);
     // close the connection:
     client.stop();
@@ -159,7 +156,8 @@ void loop() {
     
     // pause for testing
   } 
-      delay(500);
+      
+    delay(500);
 
     
     schedulers.trigger();
